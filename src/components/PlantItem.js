6428bb5572288743814364abc9d82a29e9a65600
jalips <jservice@protonmail.com>
2021-09-04 19:32:42 +0200

    Add event onClick

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,9 +1,13 @@
 import CareScale from "./CareScale";
 import '../styles/PlantItem.css'
 
+function handleClick(plantName) {
+    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
+}
+
 function PlantItem({ name, cover, id, light, water }) {
     return (
-        <li key={id} className='lmj-plant-item'>
+        <li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
             <img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
             {name}
             <div>
