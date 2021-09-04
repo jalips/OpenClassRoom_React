5abdb979ebb84d2e39dfcc7ad911f71a80815fae
jalips <jservice@protonmail.com>
2021-09-04 19:35:19 +0200

    Exemple formulaire non controler

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,19 @@ import Banner from './Banner'
 import Cart from "./Cart";
 import ShoppingList from "./ShoppingList";
 
+function handleSubmit(e) {
+  e.preventDefault()
+  alert(e.target['my_input'].value)
+}
+
 function App() {
-  return <div><Banner /><Cart /><ShoppingList /></div>
+  return <div>
+    <Banner /><Cart /><ShoppingList />
+    <form onSubmit={handleSubmit}>
+      <input type='text' name='my_input' defaultValue='Tapez votre texte' />
+      <button type='submit'>Entrer</button>
+    </form>
+  </div>
 }
 
 export default App
