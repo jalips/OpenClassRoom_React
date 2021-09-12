2b3f3bca24c8410ad53388cc74b9d57eaa0ca621
jalips <jservice@protonmail.com>
2021-09-12 16:51:11 +0200

    recopy solution

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,18 @@ import Footer from './Footer'
 import ShoppingList from './ShoppingList'
 
 function App() {
-  return (
-      <div>
-        <Banner>
-          <img src={logo} alt='La maison jungle' className='lmj-logo' />
-          <h1 className='lmj-title'>La maison jungle</h1>
-        </Banner>
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
 
-        {/* <Cart /> */}
-        <ShoppingList />
-        <Footer />
-      </div>
-  )
+			{/* <Cart /> */}
+			<ShoppingList />
+			<Footer />
+		</div>
+	)
 }
 
 export default App
