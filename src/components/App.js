367705794738057fcab2b79b4b48c610c5954446
jalips <jservice@protonmail.com>
2021-09-12 17:13:57 +0200

    stateManagement (parent -> enfant)

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,21 @@ import logo from '../assets/logo.png'
 import Cart from './Cart'
 import Footer from './Footer'
 import ShoppingList from './ShoppingList'
+import {useState} from "react";
 
 function App() {
+	const [cart, updateCart] = useState(0)
+
 	return (
 		<div>
 			<Banner>
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
-
-			<Cart />
-			<ShoppingList />
+			<div className='lmj-layout-inner'>
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
+			</div>
 			<Footer />
 		</div>
 	)
